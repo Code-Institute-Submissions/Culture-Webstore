cfe6503c31f07e6f6913633f6b8258f8b8f2fcea
Justin McCarthy <carthymcjustin@gmail.com>
2021-01-09 19:34:50 +0000

    Add featured product view and template

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from products.models import Product
 # Create your views here.
 
 def index(request):
     """A view to return the index page"""
 
-    return render(request, 'home/index.html')
+    featured = Product.objects.filter(featured=True).order_by('price')[:4]
+
+    context = {
+        'featured_products': featured,
+    }
+
+    return render(request, 'home/index.html', context)
