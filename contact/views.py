d79255a1861114153e93f3c0cc94cd4ec95945cf
Justin McCarthy <carthymcjustin@gmail.com>
2021-01-10 23:50:37 +0000

    Add contact app view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from .models import Contact
+from .forms import ContactForm
+from django.contrib import messages
 
-# Create your views here.
+def contact(request):
+    """
+    A view to add contact form info, return toast message
+    and return to same page
+    """
+
+    full_name=request.POST.get('full_name')
+    email=request.POST.get('email')
+    message=request.POST.get('message')
+    user_id=request.user
+
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            form = Contact(
+                full_name=full_name,
+                email=email,
+                message=message,
+                user_id=user_id
+            )
+            form.save()
+        else:
+            form = Contact(
+                full_name=full_name,
+                email=email,
+                message=message
+            )
+            form.save()
+
+        messages.success(request, f'Thanks for getting in touch {full_name}. We will be in contact with you soon!')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    context = {
+        'form': ContactForm
+    }
+    return render(request, 'contact/contact.html', context)
