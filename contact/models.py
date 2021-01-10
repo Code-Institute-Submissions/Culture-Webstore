dfb1450c73a427aa487922167543749f0c6e4527
Justin McCarthy <carthymcjustin@gmail.com>
2021-01-10 19:41:17 +0000

    Add contact app model and migrations

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    A contact model for storing enquiry/contact info
+    """
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    message = models.TextField()
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+    user_id = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.full_name
