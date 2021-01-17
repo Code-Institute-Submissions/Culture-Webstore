64f302bf927ce9812c01c4a33c50cf231d8f003b
Justin McCarthy <carthymcjustin@gmail.com>
2021-01-17 21:57:20 +0000

    Add privacy policy, terms and faq pages and views

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('faq/', views.faq, name='faq'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms-conditions/', views.terms_conditions, name='terms_conditions')
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,21 @@ def about(request):
     """A view to return the about page"""
 
     return render(request, 'pages/about.html')
+
+
+def faq(request):
+    """A view to return the FAQ page"""
+
+    return render(request, 'pages/faq.html')
+
+
+def privacy_policy(request):
+    """A view to return the Privacy Policy page"""
+
+    return render(request, 'pages/privacy-policy.html')
+
+
+def terms_conditions(request):
+    """A view to return the Terms & Conditions page"""
+
+    return render(request, 'pages/terms-conditions.html')
